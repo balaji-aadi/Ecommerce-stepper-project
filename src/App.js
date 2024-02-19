f4463abeb9e71cadffd0f990a87b5f53f6c8c80c
balaji-aadi <balajiaadi2000@gmail.com>
2024-02-19 16:09:43 +0530

    pagination frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import NestedFolderFileSystem from "./Projects/NestedFolderFileSystem/NestedFolderFileSystem";
+import Pagination from "./Projects/pagination/Pagination";
 import "./styles.css";
 // import PracticeWorkbase from "./Practice workbase/PracticeWorkbase";
+// import NestedFolderFileSystem from "./Projects/NestedFolderFileSystem/NestedFolderFileSystem";
 // import Stepper from "./Projects/Stepper/Stepper";
 
 export default function App() {
@@ -9,7 +10,8 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       {/* <Stepper/> */}
       {/* <PracticeWorkbase /> */}
-      <NestedFolderFileSystem />
+      {/* <NestedFolderFileSystem /> */}
+      <Pagination />
     </div>
   );
 }
---
+++ src/Projects/pagination/Pagination.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import './pagination.css'
+import axios from 'axios'
+
+const Pagination = () => {
+    const [data, setData] = useState([])
+    const [page, setPage] = useState(1)
+
+    async function fetchingApi() {
+        try {
+            const res = await axios.get("https://dummyjson.com/products?limit=100")
+            setData(res.data.products)
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchingApi()
+    }, [])
+
+    const selectPageHandler = (selectedPage) => {
+        if (selectedPage >= 1 && selectedPage <= data.length / 10 && selectedPage !== page) {
+            setPage(selectedPage)
+        }
+    }
+
+    return (
+        <div>
+            {
+                data.length > 0 && <div className='products'>
+                    {
+                        data.slice(page * 10 - 10, page * 10).map((prod) => {
+                            return <span key={prod.id} className='products__single'>
+                                <img src={prod.images[0]} alt={prod.title} />
+                                <span>{prod.title}</span>
+                            </span>
+                        })
+                    }
+                </div>
+            }
+            {
+                data.length > 0 && <div className='pagination'>
+                    <span className={page > 1 ? "" : "pagination__disable"} onClick={() => selectPageHandler(page - 1)}>◀</span>
+                    {
+                        [...Array(data.length / 10)].map((_, i) => {
+                            return <span key={i} className={page === i + 1 ? "pagination__selected" : ""} onClick={() => selectPageHandler(i + 1)}>{i + 1} </span>
+                        })
+                    }
+                    <span className={page < data.length / 10 ? "" : "pagination__disable"} onClick={() => selectPageHandler(page + 1)}>▶</span>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Pagination
